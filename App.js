09c69fd5129575ba0aa2810e4082894bdf461b68
Quynh Duong <quynhnnduong@gmail.com>
2020-08-11 23:11:53 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Constants from 'expo-constants';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+
+    );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Constants.statusBarHeight,
+        // alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: 'center',
+    },
 });
